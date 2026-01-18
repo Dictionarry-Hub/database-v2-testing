08b41ba70e3c518fe8f2e8a28e4c23d03ba96dc8
Sam Chau <schau22@pm.me>
2026-01-19 08:47:28 +1030

    test entities: nosferatu

---
+++ ops/2.create-test-entities.sql
@@ -0,0 +1,5 @@
+-- @operation: create
+-- @entity: test_entity
+-- @name: Nosferatu
+
+insert into "test_entities" ("type", "tmdb_id", "title", "year", "poster_path") values ('movie', 426063, 'Nosferatu', 2024, '/5qGIxdEO841C0tdY8vOdLoRVrr0.jpg');
