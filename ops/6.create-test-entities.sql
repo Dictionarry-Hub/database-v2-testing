6f1c2c0e22bd4c46fb313fbfaa8288f76682bc63
Sam Chau <schau22@pm.me>
2026-01-19 10:00:20 +1030

    test entities: the pitt

---
+++ ops/6.create-test-entities.sql
@@ -0,0 +1,5 @@
+-- @operation: create
+-- @entity: test_entity
+-- @name: The Pitt
+
+insert into "test_entities" ("type", "tmdb_id", "title", "year", "poster_path") values ('series', 250307, 'The Pitt', 2025, '/kvFSpESyBZMjaeOJDx7RS3P1jey.jpg');
---
+++ ops/7.import-test-releases.sql
@@ -0,0 +1,29 @@
+-- @operation: create
+-- @entity: test_release
+-- @name: 13 releases
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The Pitt S01 2160p MAX WEB-DL DDP 5.1 Atmos DV HDR H.265-FLUX', 114427512124, '["English"]', '["BeyondHD","HDBits"]', '["internal","halfleech"]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The.Pitt.S01.DV.HDR.2160p.MAX.WEB-DL.DDPA5.1.H.265-WADU', 114428770944, '["English"]', '["BroadcasTheNet"]', '["freeleech"]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The Pitt 2025 S01 2160p MAX WEB-DL DD+ 5.1 Atmos DV HDR H.265-WADU', 114428772352, '["English"]', '["Blutopia"]', '[]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The Pitt S01 REPACK 2160p MAX WEB-DL DDP 5.1 Atmos H.265-FLUX', 96659473125, '["English"]', '["BeyondHD"]', '["internal"]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The.Pitt.S01.2160p.MAX.WEB-DL.DDP5.1.H.265-NTb', 96648814458, '["English"]', '["BroadcasTheNet","HDBits","Blutopia"]', '["freeleech","internal","halfleech"]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The.Pitt.S01.DV.2160p.MAX.WEB-DL.DDPA5.1.H.265-FLUX', 104595321252, '["English"]', '["BroadcasTheNet","BeyondHD"]', '["freeleech","internal"]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The Pitt S01 REPACK 1080p AMZN WEB-DL DDP 5.1 Atmos H.264-FLUX', 47072074171, '["English"]', '["BeyondHD","Blutopia"]', '["internal"]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The.Pitt.S01.1080p.AMZN.WEB-DL.DDP5.1.H.264-NTb', 47077636284, '["English"]', '["BroadcasTheNet","HDBits"]', '["freeleech","internal","halfleech"]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The.Pitt.S01.1080p.MAX.WEB-DL.DDP5.1.H.264-NTb', 20162000206, '["English"]', '["BroadcasTheNet","BeyondHD","Blutopia"]', '["freeleech","internal"]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The Pitt S01 REPACK 720p AMZN WEB-DL DDP 5.1 Atmos H.264-FLUX', 22523297186, '["English"]', '["BeyondHD"]', '["internal"]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The.Pitt.S01.720p.AMZN.WEB-DL.DDP5.1.H.264-NTb', 22528382579, '["English"]', '["BroadcasTheNet","HDBits","Blutopia"]', '["freeleech","internal","halfleech"]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The.Pitt.S01.720p.MAX.WEB-DL.DDP5.1.H.264-NTb', 11772406415, '["English"]', '["BroadcasTheNet","Blutopia"]', '["freeleech","internal"]');
+
+insert into "test_releases" ("entity_type", "entity_tmdb_id", "title", "size_bytes", "languages", "indexers", "flags") values ('series', 250307, 'The.Pitt.S01.480p.MAX.WEB-DL.DDP5.1.H.264-REQ', 4752625308, '["English"]', '["BroadcasTheNet"]', '["freeleech"]');
