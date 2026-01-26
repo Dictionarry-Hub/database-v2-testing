79581b2432d18d7381b5db17e7c1e4944fa2f6da
Sam Chau <schau22@pm.me>
2026-01-27 07:45:51 +1030

    test

---
+++ ops/15.create-radarr-media-settings-test.sql
@@ -0,0 +1,5 @@
+-- @operation: create
+-- @entity: radarr_media_settings
+-- @name: test
+
+insert into "radarr_media_settings" ("name", "propers_repacks", "enable_media_info") values ('test', 'doNotPrefer', 1);
---
+++ ops/16.create-sonarr-media-settings-test.sql
@@ -0,0 +1,5 @@
+-- @operation: create
+-- @entity: sonarr_media_settings
+-- @name: test
+
+insert into "sonarr_media_settings" ("name", "propers_repacks", "enable_media_info") values ('test', 'doNotPrefer', 1);
