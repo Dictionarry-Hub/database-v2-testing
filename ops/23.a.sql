75527076928c27c3e234a5f86b97ab1c90d7def3
a <a>
2026-02-02 05:02:59 +1030

    a

---
+++ ops/23.a.sql
@@ -0,0 +1,19 @@
+-- @operation: export
+-- @entity: batch
+-- @name: a
+-- @exportedAt: 2026-02-01T18:32:58.713Z
+-- @opIds: 358
+
+-- --- BEGIN op 358 ( update quality_profile "2160p Balanced" )
+update "quality_profiles" set "description" = '2160p Balanced targets consistent & immutable 2160p **WEB-DLs w/ Lossy Audio**. 
+
+- Average Movie Sizes ~ 15 to 30gb per Movie
+- Movie Quality Ranking ~ 8/10
+- Average TV Sizes ~ 5 to 15gb per Episode
+- TV Quality Ranking ~ 8/10a' where "name" = '2160p Balanced' and "description" = '2160p Balanced targets consistent & immutable 2160p **WEB-DLs w/ Lossy Audio**. 
+
+- Average Movie Sizes ~ 15 to 30gb per Movie
+- Movie Quality Ranking ~ 8/10
+- Average TV Sizes ~ 5 to 15gb per Episode
+- TV Quality Ranking ~ 8/10';
+-- --- END op 358
