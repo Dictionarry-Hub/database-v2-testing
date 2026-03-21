07d2d732130708f624acf9052b4e537a4111e687
test <test@test.com>
2026-03-21 13:31:45 +1030

    test

---
+++ ops/2.test.sql
@@ -0,0 +1,19 @@
+-- @operation: export
+-- @entity: batch
+-- @name: test
+-- @exportedAt: 2026-03-21T03:01:39.384Z
+-- @opIds: 2026
+
+-- --- BEGIN op 2026 ( update quality_profile "1080p Balanced" )
+update "quality_profiles" set "description" = '1080p Balanced targets consistent & immutable 1080p **WEB-DLs** using the Streaming Source and Audio Formats to determine the level of Transparency.
+
+- Average Movie Sizes ~ 4 to 8gb per Movie
+- Movie Quality Ranking ~ 6/10
+- Average TV Sizes ~ 2 to 4gb per Episode
+- TV Quality Ranking ~ 7/10 test' where "name" = '1080p Balanced' and "description" = '1080p Balanced targets consistent & immutable 1080p **WEB-DLs** using the Streaming Source and Audio Formats to determine the level of Transparency.
+
+- Average Movie Sizes ~ 4 to 8gb per Movie
+- Movie Quality Ranking ~ 6/10
+- Average TV Sizes ~ 2 to 4gb per Episode
+- TV Quality Ranking ~ 7/10';
+-- --- END op 2026
